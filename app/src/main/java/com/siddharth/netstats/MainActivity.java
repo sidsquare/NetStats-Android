38b629261982b4bdcc2e3222525a349f71cc5862
chrome289 <sidat289@gmail.com>
2015-01-29 19:28:10 +0530

    Speed and Data usage working

--- app/src/main/java/com/siddharth/netstats/MainActivity.java
+++ app/src/main/java/com/siddharth/netstats/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends ActionBarActivity
 {
     private Handler handler = new Handler();
-    private long rx,tx;
+    private long rx,tx,temp_rx,temp_tx;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,18 +21,20 @@ public class MainActivity extends ActionBarActivity
         rx=TrafficStats.getTotalRxBytes();rx=rx/(1024);
         tx=TrafficStats.getTotalTxBytes();tx=tx/(1024);
         TextView t1=(TextView)findViewById(R.id.tv1);
-        String temp;
-        temp = "Download "+Long.toString(rx)+" KB";
-        t1.setText(temp);
-        TextView t2=(TextView)findViewById(R.id.tv2);
-        temp = "Upload "+Long.toString(tx)+" KB";
-        t2.setText(temp);
+        t1.setText("0 KB");
+        t1=(TextView)findViewById(R.id.tv2);
+        t1.setText("0 KB");
+        t1=(TextView)findViewById(R.id.tv3);
+        t1.setText("0 KBPS");
+        t1=(TextView)findViewById(R.id.tv4);
+        t1.setText("0 KBPS");
+        temp_tx=tx;temp_rx=rx;
         prog();
     }
 
     private void prog()
     {
-        handler.postDelayed(runnable, 100);
+        handler.postDelayed(runnable, 1000);
 
     }
     private Runnable runnable = new Runnable() {
@@ -40,17 +42,23 @@ public class MainActivity extends ActionBarActivity
         public void run() {
             long rx1=TrafficStats.getTotalRxBytes();rx1=rx1/(1024);
             long tx1=TrafficStats.getTotalTxBytes();tx1=tx1/(1024);
-            long down_speed=rx1-rx,up_speed=rx1-rx;
-            rx=rx1;
-            tx=tx1;
-            TextView t1=(TextView)findViewById(R.id.tv1);
             String temp;
-            temp = "Download Speed "+Long.toString(down_speed)+" KB";
+            long down_speed=rx1-temp_rx,up_speed=tx1-temp_tx,down_data=rx1-rx,up_data=tx1-tx;
+            
+            TextView t1=(TextView)findViewById(R.id.tv1);
+            temp=Long.toString(down_data)+" KB";
             t1.setText(temp);
-            TextView t2=(TextView)findViewById(R.id.tv2);
-            temp = "Upload Speed "+Long.toString(up_speed)+" KB";
-            t2.setText(temp);
-            rx=rx1;tx=tx1;
+            t1=(TextView)findViewById(R.id.tv2);
+            temp=Long.toString(up_data)+" KB";
+            t1.setText(temp);
+            t1=(TextView)findViewById(R.id.tv3);
+            temp=Long.toString(down_speed)+" KBPS";
+            t1.setText(temp);
+            t1=(TextView)findViewById(R.id.tv4);
+            temp=Long.toString(up_speed)+" KBPS";
+            t1.setText(temp);
+
+            temp_tx=tx1;temp_rx=rx1;
             handler.postDelayed(this, 1000);
         }
     };
