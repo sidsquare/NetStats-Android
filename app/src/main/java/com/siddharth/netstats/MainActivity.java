114500f4a14eb985d0e4355b99912367f95da64b
chrome289 <sidat289@gmail.com>
2015-01-30 23:38:28 +0530

    Dumped Fragment and used activities

--- app/src/main/java/com/siddharth/netstats/MainActivity.java
+++ app/src/main/java/com/siddharth/netstats/MainActivity.java
@@ -1,75 +1,73 @@
 package com.siddharth.netstats;
 
+import android.content.Intent;
 import android.net.TrafficStats;
 import android.os.Bundle;
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Handler;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
-    String[] menu;
-    public Handler handler = new Handler();
-    public long rx,tx,temp_rx,temp_tx;
-    DrawerLayout dLayout;
-    ListView dList;
-    ArrayAdapter<String> adapter;
+    private Handler handler = new Handler();
+    private long rx,tx,temp_rx,temp_tx;
+    private ListView listView;private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        menu = new String[]{"Home","Charts"};
-        dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        dList = (ListView) findViewById(R.id.left_drawer);
-        adapter = new ArrayAdapter<String>(this, R.layout.nav_menu,R.id.textview,menu);
-        dList.setAdapter(adapter);
-        dList.setSelector(android.R.color.holo_blue_dark);
-        dList.setOnItemClickListener(new OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
-                dLayout.closeDrawers();
-                if(position==0) {
-                    cfrag1 newFragment = new cfrag1();
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.content_frame, newFragment);
-                    transaction.commit();
-                }
-                else{
-                    cfrag2 newFragment = new cfrag2();
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.content_frame, newFragment);
-                    transaction.commit();
-                }
-            }
-        });
+
+
+        String[] countryArray = {"Data", "Charts"};
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.nav_menu,R.id.textview, countryArray);
+        listView = (ListView) findViewById(R.id.left_drawer);
+        listView.setAdapter(adapter);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listView.setOnItemClickListener(new DrawerItemClickListener());
+
         rx=TrafficStats.getTotalRxBytes();rx=rx/(1024);
         tx=TrafficStats.getTotalTxBytes();tx=tx/(1024);
-        /*TextView t1=(TextView)findViewById(R.id.tv1);
+        TextView t1=(TextView)findViewById(R.id.tv1);
         t1.setText("0 KB");
         t1=(TextView)findViewById(R.id.tv2);
         t1.setText("0 KB");
         t1=(TextView)findViewById(R.id.tv3);
         t1.setText("0 KBPS");
         t1=(TextView)findViewById(R.id.tv4);
-        t1.setText("0 KBPS");*/
+        t1.setText("0 KBPS");
         temp_tx=tx;temp_rx=rx;
-        //prog();
+        prog();
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+            mDrawerLayout.closeDrawer(listView);
+            listView.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void selectItem(int position)
+    {
+        if (position ==1)
+        {
+            Intent t = new Intent(this, cfrag2.class);
+
+            startActivity(t);
+            overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+        }
+    }
+
+
     private void prog()
     {
         handler.postDelayed(runnable, 1000);
@@ -100,10 +98,28 @@ public class MainActivity extends ActionBarActivity {
         }
     };
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/siddharth/netstats/cfrag2.java
+++ app/src/main/java/com/siddharth/netstats/cfrag2.java
@@ -1,23 +1,76 @@
 package com.siddharth.netstats;
 
-
-
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class cfrag2 extends Fragment {
-    final static String ARG_POSITION = "position";
-    int mCurrentPosition = -1;
 
+public class cfrag2 extends ActionBarActivity {
+    private ListView listView;private DrawerLayout mDrawerLayout;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frag2);
 
-        return inflater.inflate(R.layout.frag2, container, false);
+        String[] countryArray = {"Data", "Charts"};
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.nav_menu,R.id.textview, countryArray);
+        listView = (ListView) findViewById(R.id.left_drawer);
+        listView.setAdapter(adapter);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listView.setOnItemClickListener(new DrawerItemClickListener());
+    }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+            mDrawerLayout.closeDrawer(listView);
+            listView.setVisibility(View.INVISIBLE);
+        }
     }
 
+    private void selectItem(int position)
+    {
+        if (position == 0)
+        {
+
+            Intent t = new Intent(this, MainActivity.class);
+            startActivity(t);
+
+            overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
