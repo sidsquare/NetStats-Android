5e77557926a17a51c3b3b0073aa2544231445459
chrome289 <sidat289@gmail.com>
2015-01-31 18:13:56 +0530

    Did it !!!

--- app/src/main/java/com/siddharth/netstats/MainActivity.java
+++ app/src/main/java/com/siddharth/netstats/MainActivity.java
@@ -1,125 +1,129 @@
 package com.siddharth.netstats;
 
-import android.content.Intent;
 import android.net.TrafficStats;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
-    private Handler handler = new Handler();
-    private long rx,tx,temp_rx,temp_tx;
-    private ListView listView;private DrawerLayout mDrawerLayout;
+public class MainActivity extends ActionBarActivity
+{
+    boolean cfrag1_is_enabled = false,first_time=true;
+    String[] menu;
+    String temp1="0 KB", temp2="0 KB", temp3="0 KBPS", temp4="0 KBPS";
+    public Handler handler = new Handler();
+    public long rx, tx, temp_rx, temp_tx;
+    DrawerLayout dLayout;
+    ListView dList;
+    ArrayAdapter<String> adapter;
+    cfrag1 frag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        menu = new String[]{"Home", "Charts"};
+        dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        dList = (ListView) findViewById(R.id.left_drawer);
+        adapter = new ArrayAdapter<String>(this, R.layout.nav_menu, R.id.textview, menu);
+        dList.setAdapter(adapter);
+        dList.setSelector(android.R.color.holo_blue_dark);
 
-
-        String[] countryArray = {"Data", "Charts"};
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.nav_menu,R.id.textview, countryArray);
-        listView = (ListView) findViewById(R.id.left_drawer);
-        listView.setAdapter(adapter);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        listView.setOnItemClickListener(new DrawerItemClickListener());
-
-        rx=TrafficStats.getTotalRxBytes();rx=rx/(1024);
-        tx=TrafficStats.getTotalTxBytes();tx=tx/(1024);
-        TextView t1=(TextView)findViewById(R.id.tv1);
-        t1.setText("0 KB");
-        t1=(TextView)findViewById(R.id.tv2);
-        t1.setText("0 KB");
-        t1=(TextView)findViewById(R.id.tv3);
-        t1.setText("0 KBPS");
-        t1=(TextView)findViewById(R.id.tv4);
-        t1.setText("0 KBPS");
-        temp_tx=tx;temp_rx=rx;
-        prog();
-    }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView parent, View view, int position, long id) {
-            selectItem(position);
-            mDrawerLayout.closeDrawer(listView);
-            listView.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    private void selectItem(int position)
-    {
-        if (position ==1)
+        dList.setOnItemClickListener(new OnItemClickListener()
         {
-            Intent t = new Intent(this, cfrag2.class);
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View v, int position, long id)
+            {
+                dLayout.closeDrawers();
+                if (position == 0)
+                {
+                    cfrag1 newFragment = new cfrag1();
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content_frame, newFragment, "cfrag1");
+                    //ft.addToBackStack("cfrag1");
+                    ft.commit();
+                    getSupportFragmentManager().executePendingTransactions();   //fucking important
 
-            startActivity(t);
-            overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
-        }
+                    frag = (cfrag1) getSupportFragmentManager().findFragmentByTag("cfrag1");
+                    if (frag != null)
+                    {
+                        cfrag1_is_enabled = true;
+                        if(first_time==true)
+                        {
+                            rx = TrafficStats.getTotalRxBytes();
+                            rx = rx / (1024);
+                            tx = TrafficStats.getTotalTxBytes();
+                            tx = tx / (1024);
+                            temp_tx = tx;
+                            temp_rx = rx;
+                            frag.go(temp1, temp2, temp3, temp4);
+                            prog();
+                            first_time=false;
+                        }
+                        else
+                            frag.go(temp1, temp2, temp3, temp4);
+                    }
+                }
+                else
+                {
+                    cfrag1_is_enabled = false;
+                    cfrag2 newFragment = new cfrag2();
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content_frame, newFragment, "cfrag2");
+                    //ft.addToBackStack("cfrag2");
+                    ft.commit();
+                    getSupportFragmentManager().executePendingTransactions();
+                }
+            }
+        });
     }
-
 
     private void prog()
     {
+        first_time=false;
         handler.postDelayed(runnable, 1000);
     }
-    private Runnable runnable = new Runnable() {
+
+    private Runnable runnable = new Runnable()
+    {
         @Override
-        public void run() {
-            long rx1=TrafficStats.getTotalRxBytes();rx1=rx1/(1024);
-            long tx1=TrafficStats.getTotalTxBytes();tx1=tx1/(1024);
-            String temp;
-            long down_speed=rx1-temp_rx,up_speed=tx1-temp_tx,down_data=rx1-rx,up_data=tx1-tx;
+        public void run()
+        {
+            long rx1 = TrafficStats.getTotalRxBytes();
+            rx1 = rx1 / (1024);
+            long tx1 = TrafficStats.getTotalTxBytes();
+            tx1 = tx1 / (1024);
+            long down_speed = rx1 - temp_rx, up_speed = tx1 - temp_tx, down_data = rx1 - rx, up_data = tx1 - tx;
 
-            TextView t1=(TextView)findViewById(R.id.tv1);
-            temp=Long.toString(down_data)+" KB";
-            t1.setText(temp);
-            t1=(TextView)findViewById(R.id.tv2);
-            temp=Long.toString(up_data)+" KB";
-            t1.setText(temp);
-            t1=(TextView)findViewById(R.id.tv3);
-            temp=Long.toString(down_speed)+" KBPS";
-            t1.setText(temp);
-            t1=(TextView)findViewById(R.id.tv4);
-            temp=Long.toString(up_speed)+" KBPS";
-            t1.setText(temp);
+            temp1 = Long.toString(down_data) + " KB";
+            temp2 = Long.toString(up_data) + " KB";
+            temp3 = Long.toString(down_speed) + " KBPS";
+            temp4 = Long.toString(up_speed) + " KBPS";
 
-            temp_tx=tx1;temp_rx=rx1;
+            temp_tx = tx1;
+            temp_rx = rx1;
+            if (cfrag1_is_enabled == true)
+                frag.go(temp1, temp2, temp3, temp4);
             handler.postDelayed(this, 1000);
         }
     };
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 }
--- app/src/main/java/com/siddharth/netstats/cfrag1.java
+++ app/src/main/java/com/siddharth/netstats/cfrag1.java
@@ -1,17 +1,33 @@
 package com.siddharth.netstats;
 
-import android.app.Fragment;
+
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+public class cfrag1 extends Fragment {
 
-public class cfrag1 extends ActionBarActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.frag1);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.frag1, container, false);
+    }
+    public void go(String temp1,String temp2,String temp3,String temp4)
+    {
+        Log.v("gdfg", "begin");
+        TextView t1=(TextView)getView().findViewById(R.id.tv1);
+        t1.setText(temp1);
+        t1=(TextView)getView().findViewById(R.id.tv2);
+        t1.setText(temp2);
+        t1=(TextView)getView().findViewById(R.id.tv3);
+        t1.setText(temp3);
+        t1=(TextView)getView().findViewById(R.id.tv4);
+        t1.setText(temp4);
+        Log.v("dsfs", "end");
     }
 }
